8b31f85ff2cb0cd5352ef71729454c7d95bdea2c
meow <damirzhum345@gmail.com>
2024-11-14 13:22:53 +0300

    polya

--- Demo/DemoWin/ClientInfo.Designer.cs
+++ Demo/DemoWin/ClientInfo.Designer.cs
@@ -30,6 +30,15 @@
         {
             this.CompanyNameTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBoxDiscount = new System.Windows.Forms.TextBox();
+            this.textBoxRating = new System.Windows.Forms.TextBox();
+            this.textBoxDirector = new System.Windows.Forms.TextBox();
+            this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // CompanyNameTextBox
@@ -44,17 +53,107 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(121, 64);
+            this.label1.Location = new System.Drawing.Point(121, 67);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(104, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Название клиента";
+            // 
+            // textBoxDiscount
+            // 
+            this.textBoxDiscount.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxDiscount.Location = new System.Drawing.Point(231, 176);
+            this.textBoxDiscount.Name = "textBoxDiscount";
+            this.textBoxDiscount.Size = new System.Drawing.Size(100, 22);
+            this.textBoxDiscount.TabIndex = 2;
+            // 
+            // textBoxRating
+            // 
+            this.textBoxRating.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxRating.Location = new System.Drawing.Point(231, 148);
+            this.textBoxRating.Name = "textBoxRating";
+            this.textBoxRating.Size = new System.Drawing.Size(100, 22);
+            this.textBoxRating.TabIndex = 3;
+            // 
+            // textBoxDirector
+            // 
+            this.textBoxDirector.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxDirector.Location = new System.Drawing.Point(231, 120);
+            this.textBoxDirector.Name = "textBoxDirector";
+            this.textBoxDirector.Size = new System.Drawing.Size(100, 22);
+            this.textBoxDirector.TabIndex = 4;
+            // 
+            // PhoneNumberTextBox
+            // 
+            this.PhoneNumberTextBox.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PhoneNumberTextBox.Location = new System.Drawing.Point(231, 92);
+            this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+            this.PhoneNumberTextBox.Size = new System.Drawing.Size(100, 22);
+            this.PhoneNumberTextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(121, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Скидка";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(121, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Рейтинг";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(121, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Имя директора";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(121, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Номер телефона";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(256, 204);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 10;
+            this.SaveButton.Text = "Сохранить";
+            this.SaveButton.UseVisualStyleBackColor = true;
             // 
             // ClientInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PhoneNumberTextBox);
+            this.Controls.Add(this.textBoxDirector);
+            this.Controls.Add(this.textBoxRating);
+            this.Controls.Add(this.textBoxDiscount);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.CompanyNameTextBox);
             this.Name = "ClientInfo";
@@ -67,5 +166,14 @@
 
         private System.Windows.Forms.TextBox CompanyNameTextBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxDiscount;
+        private System.Windows.Forms.TextBox textBoxRating;
+        private System.Windows.Forms.TextBox textBoxDirector;
+        private System.Windows.Forms.TextBox PhoneNumberTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button SaveButton;
     }
 }
--- Demo/DemoWin/ClientInfo.cs
+++ Demo/DemoWin/ClientInfo.cs
@@ -1,4 +1,5 @@
 ﻿using DemoLib;
+using System;
 using System.Windows.Forms;
 
 namespace Demo
@@ -13,6 +14,10 @@ namespace Demo
         public void SetClient(Client client)
         {
             CompanyNameTextBox.Text = client.Name;
+            PhoneNumberTextBox.Text = client.PhoneNumber;
+            textBoxDirector.Text = client.Director;
+            textBoxRating.Text = client.Rating.ToString();
+            textBoxDiscount.Text = Convert.ToString(client.Discount);
         }
     }
 }
